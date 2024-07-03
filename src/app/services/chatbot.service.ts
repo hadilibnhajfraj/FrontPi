ce5e374ae8749a66927680396de2f9e02dae6e70
bhf hadil <hadil.ibnhajfraj@esprit.tn>
2024-07-03 23:12:18 +0100

    fix error

--- src/app/services/chatbot.service.ts
+++ src/app/services/chatbot.service.ts
@@ -1,22 +1,19 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ChatService {
-  
-  private apiUrl = 'http://localhost:3000/cours/chat';
+  private apiUrl = "http://localhost:3000/cours/chat";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   postQuestion(question: string) {
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     });
 
     return this.http.post<any>(this.apiUrl, { question }, { headers });
   }
-
-
 }
